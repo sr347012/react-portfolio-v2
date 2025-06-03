9d82264b68d250d1669d0978aa23e15d9d2034bb
Sriram <sriramachandra2387.k@gmail.com>
2025-06-03 13:28:51 +0530

    updated images

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ function About() {
 
         <div className="about-desc">
           <p>
-            Myself Sriram. I have 15+ years of experience in IT industry. I have
+            Myself Sriram. I have 16+ years of experience in IT industry. I have
             been associated with multiple organisations such as Tata Consultancy
             services, Collabera, CGI and Wipro technologies. I have rendered my
             services to major clients such as American Express, Walmart, Shell
@@ -19,10 +19,10 @@ function About() {
           </p>
           <br />
           <p>
-            I Have been into multiple technologies such as React, Angular,
-            Javascript, Next JS, GraphQL. I had exposure to cloud technologies
-            such as AWS, GCP & IBM Cloud. I also had experience with databases such as
-            DB2, Mongo DB and PostgreSQL.
+            I Have been into multiple web technologies such as React, Next
+            JS, Typescript, Angular, Javascript, GraphQL. I had exposure to cloud
+            technologies such as AWS, GCP & IBM Cloud. I also had experience
+            with databases such as DB2, Mongo DB and PostgreSQL.
           </p>
           <br />
           <p>
@@ -32,8 +32,8 @@ function About() {
             did my masters in data science and engineering. I continued towards
             doing research in computer vision and enrolled for a PhD in 2023. I
             participate in ML & AI hackathons and explore multiple
-            opportunities. I explored prompt engineering & GenAI and
-            built multiple projects based on them.
+            opportunities. I explored prompt engineering & GenAI and built
+            multiple projects based on them.
           </p>
           <br />
           <p>
