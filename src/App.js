b6d463698037916dbe8a844e1ca890763225942a
Sriram <sriramachandra2387.k@gmail.com>
2026-03-08 23:19:52 +0530

    fix researxh

--- src/App.js
+++ src/App.js
@@ -49,7 +49,6 @@ function App() {
       <About />
       <hr></hr>
 
-
       <Footer />
     </>
   );
--- src/components/Research.js
+++ src/components/Research.js
@@ -1,9 +1,71 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import AddIcon from "@mui/icons-material/Add";
 
 function Research() {
   return (
     <>
       <div className="history-heading">Research</div>
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          "& > :not(style)": {
+            m: 2,
+            mb: "50px",
+            width: 200,
+            height: 200,
+            borderRadius: "50%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            // bgcolor: "#EF9166",
+            fontSize: "24px",
+            // boxShadow: "2px 3px 5px 3px white, -3px -1px 3px 3px #EF9166",
+          },
+        }}
+      >
+        <Paper
+          className="cs-pointer"
+          sx={{
+            bgcolor: "#EF9166",
+            boxShadow: "2px 3px 5px 3px white, -3px -1px 3px 3px #EF9166",
+          }}
+        >
+          Egocentric vision
+        </Paper>
+        <AddIcon
+          sx={{
+            color: "#EF9166",
+            // boxShadow: "2px 3px 5px 3px white, -3px -1px 3px 3px #EF9166",
+          }}
+        />
+        <Paper
+          className="cs-pointer"
+          sx={{
+            bgcolor: "#EF9166",
+            boxShadow: "2px 3px 5px 3px white, -3px -1px 3px 3px #EF9166",
+          }}
+        >
+          Badminton
+        </Paper>
+        <AddIcon
+          sx={{
+            color: "#EF9166",
+            fontSize: "14px",
+          }}
+        />
+        <Paper
+          className="cs-pointer"
+          sx={{
+            bgcolor: "#EF9166",
+            boxShadow: "2px 3px 5px 3px white, -3px -1px 3px 3px #EF9166",
+          }}
+        >
+          Grip Dynamics
+        </Paper>
+      </Box>
     </>
   );
 }
